07cbf3b6478ac1425a0b85c8e175f6ad1ee0b4b9
Israel Sagesse <israelsagesse@gmail.com>
2021-04-20 22:25:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+age = int(input("What is your current age?"))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+days = (90 - age) * 365
+
+weeks = (90 - age) * 52
+
+months = (90 - age) * 12
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left to live till you turn 90 years old. ")
+
+
+
+
+
+
+
